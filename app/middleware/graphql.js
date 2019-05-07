f976ad40cef128c689f224fb07691db8566ba1d4
supperchong <supperchong@gmail.com>
2019-05-07 23:25:50 +0800

    format

--- app/middleware/graphql.js
+++ app/middleware/graphql.js
@@ -1,40 +1,40 @@
 'use strict';
 
 
-const { ApolloServer } = require('apollo-server-koa')
-const compose = require('koa-compose')
+const { ApolloServer } = require('apollo-server-koa');
+const compose = require('koa-compose');
 /**
  * @param {Object} options The `options` of apollo-server.
  * @return {Promise} The compose of middleware in apollo-server-koa.
  */
 
 module.exports = (_, app) => {
-  let graphqlConfig=app.config.graphql
-  if(typeof graphqlConfig==='function'){
-    graphqlConfig=graphqlConfig(app)
+  let graphqlConfig = app.config.graphql;
+  if (typeof graphqlConfig === 'function') {
+    graphqlConfig = graphqlConfig(app);
   }
-  const options = {...app.schemaConfig,...graphqlConfig}
-  const {graphiql=true,router,...ApolloServerConfig}=options
+  const options = { ...app.schemaConfig, ...graphqlConfig };
+  const { graphiql = true, router, ...ApolloServerConfig } = options;
   const server = new ApolloServer({
-    context: options=>options.ctx,
-    //不设置request.credentials 会导致请求不带cookie
-    playground:graphiql&&{
-      settings:{
-        "request.credentials": "include"
-      }
+    context: options => options.ctx,
+    // 不设置request.credentials 会导致请求不带cookie
+    playground: graphiql && {
+      settings: {
+        'request.credentials': 'include',
+      },
     },
     ...ApolloServerConfig,
-  })
-  
-  const middlewares = []
+  });
+
+  const middlewares = [];
   const proxyApp = {
     use: m => {
-      middlewares.push(m)
+      middlewares.push(m);
     },
-  }
+  };
   server.applyMiddleware({
     app: proxyApp,
-    path:router
-  })
-  return compose(middlewares)
-}
+    path: router,
+  });
+  return compose(middlewares);
+};
