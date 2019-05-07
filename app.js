f503de3c732ba70e7022a51425a4830e5c652296
supperchong <supperchong@gmail.com>
2019-05-07 23:24:03 +0800

    refactor and support ts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = app => {
-  require('./lib/load_schema')(app);
+module.exports = async app => {
+  await require('./lib/load_schema')(app);
   require('./lib/load_connector')(app);
 };
